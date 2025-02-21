1a2d446bff12124fdf0fd515d1e9d75a1893bb8d
Zack Moore <39570657+sqlzack@users.noreply.github.com>
2025-02-21 20:35:19 +0000

    Exercise 05

--- src/ContosoSuitesWebAPI/Services/DatabaseService.cs
+++ src/ContosoSuitesWebAPI/Services/DatabaseService.cs
@@ -17,7 +17,7 @@ public class DatabaseService(string connectionString) : IDatabaseService
     /// Get all hotels from the database.
     /// </summary>
 
-    [KernelFunction]
+    [KernelFunction("get_hotels")]
     [Description("Get all hotels.")]
     public async Task<IEnumerable<Hotel>> GetHotels()
     {
